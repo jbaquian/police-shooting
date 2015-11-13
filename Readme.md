325100e9103618508e5f4c2474ca9cd3f8973177
jacob <jlb959@uw.edu>
2015-11-13 00:05:46 -0800

    First update

--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
-# Police shootings
-
-This repository is a challenge for the following [course](http://faculty.washington.edu/mikefree/info343/) at the University of Washington.  The instructions are described [here](http://faculty.washington.edu/mikefree/info343/#/challenges/map-challenge).
+1. The purpose of this project is to use leaflet and open space maps to show the police shootings around the world and sort them by gender and how serious the shooting was.
+2.http://students.washington.edu/jlb959/info343/police-shooting
+3.Your slides from class time and the internet
+4.10hrs
+5.I had trouble getting the circles to load on my map
--- js/map.js
+++ js/map.js
@@ -1,35 +1,73 @@
-// Function to draw your map
-var drawMap = function() {
-
-  // Create map and set view
- 
-
-  // Create a tile layer variable using the appropriate url
-
-
-  // Add the layer to your map
- 
-
-  // Execute your function to get data
- 
-}
-
-// Function for getting data
-var getData = function() {
-
-  // Execute an AJAX request to get the data in data/response.js
-
-
-  // When your request is successful, call your customBuild function
+var drawMap = function(){
+	var map = L.map('map').setView([38, -99], 4);
+	var layer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+    layer.addTo(map);
+    getData();
 
 }
 
-// Loop through your data and add the appropriate layers and points
-var customBuild = function() {
-	// Be sure to add each layer to the map
-
-	// Once layers are on the map, add a leaflet controller that shows/hides layers
-  
+var getData = function(){
+	$.get('data/response.json', customBuild)
 }
 
+var customBuild = function(data){
+	var dataSet = $.parseJSON(data);
 
+	var maleHit = 0;
+	var maleKilled = 0;
+	var femaleHit = 0;
+	var femaleKilled = 0;
+
+	var male = L.layerGroup([]);
+	var female = L.layerGroup([]);
+
+	var layerId = {
+		"male":male,
+		"female":female
+	}
+
+	dataSet.map(function(shooting){
+		var x;
+		var gender = shooting["Victim's Gender"];
+		var outcome = shooting["Hit or Killed"];
+		var circleColor;
+
+		if (outcome == "Hit") {
+			circleColor = "red";
+		} else {
+			circleColor = "black";
+		}
+
+		var circle = new L.circleMarker([shooting.lat, shooting.lng], {color :  circleColor});
+		console.log(circle);
+
+		if(gender == "Male"){
+			if(outcome == "Hit"){
+				malesHit++;
+			} else {
+				malesKilled++;
+			}
+		} else {
+			if(outcome =="Hit") {
+				femaleHit++;
+			} else {
+				femaleKilled++;
+			}
+		}
+		console.log(maleHit);
+		circle.bindPopup(shooting.Summary);
+		circle.addTo(x)
+	})
+
+	$("#male").append("<td>" + maleHit +"</td>")
+	$("#male").append("<td>" + maleKilled +"</td>")
+	$("#female").append("<td>" + femaleHit +"</td>")
+	$("#female").append("<td>" + femaleKilled +"</td>")
+
+	male.addTo(map);
+	female.addTo(map);
+
+	var layers = {"Male": male, "Female":female}
+
+	L.control.layers(null, layers).addTo(map);
+}
